6f50da71a89146de7cc407d28a026a1e2c4bf567
Rafael Ostertag <rafi@guengel.ch>
2020-08-09 15:45:13 +0200

    Add Prometheus metrics

---
+++ src/main/kotlin/modules/Metrics.kt
@@ -0,0 +1,26 @@
+package ch.guengel.imageserver.modules
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.metrics.micrometer.MicrometerMetrics
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+
+
+fun Application.setupMetrics() {
+    val registry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    install(MicrometerMetrics) {
+        this.registry = registry
+    }
+
+    routing {
+        get("/prometheus") {
+            val result = registry.scrape()
+            call.respond(result)
+        }
+    }
+}
