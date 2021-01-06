6801768ea261abf18782c38d1a9bacfb846812c1
Rafael Ostertag <rafi@guengel.ch>
2021-01-06 20:44:42 +0100

    Log clients

--- src/main/kotlin/ch/guengel/imageserver/modules/CallLogging.kt
+++ src/main/kotlin/ch/guengel/imageserver/modules/CallLogging.kt
@@ -2,10 +2,20 @@ package ch.guengel.imageserver.modules
 
 import io.ktor.application.*
 import io.ktor.features.*
+import io.ktor.request.*
 import org.slf4j.event.Level
 
 fun Application.setupCallLogging() {
     install(CallLogging) {
         level = Level.INFO
+        format { call ->
+            val path = call.request.uri
+            val method = call.request.httpMethod.value
+            val status = call.response.status()
+            val host = call.request.host()
+            val realIp = call.request.headers["X-Real-IP"]
+            val forwardedFor = call.request.headers["X-Forwarded-For"]
+            "[$host] $method $path $status (X-Real-IP: $realIp;X-Forwarded-For: $forwardedFor)"
+        }
     }
 }
